0140ae15e8de4e0ff3c3bc5cde3b846dc70f0f63
Aritra Ghosh ⚡ <95587262+TheCleverIdiott@users.noreply.github.com>
2023-11-17 20:50:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ def extract_text_from_pdf(file_path):
 choice = st.sidebar.selectbox("Select your choice", ["Summarize Text", "Summarize Document"])
 
 if choice == "Summarize Text":
-    st.subheader("Summarize Text using txtai")
+    st.subheader("Summarize Text")
+    st.write("Made by Aritra Ghosh (itsaritra.pages.dev)")
     input_text = st.text_area("Enter your text here")
     if input_text is not None:
         if st.button("Summarize Text"):
@@ -37,7 +38,8 @@ if choice == "Summarize Text":
                 st.success(result)
 
 elif choice == "Summarize Document":
-    st.subheader("Summarize Document using txtai")
+    st.subheader("Summarize Document")
+    st.write("Made by Aritra Ghosh (itsaritra.pages.dev)")
     input_file = st.file_uploader("Upload your document here", type=['pdf'])
     if input_file is not None:
         if st.button("Summarize Document"):
